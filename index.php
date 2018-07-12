e345ee8c61b2806ed61be561e83c20703819a401
Ammar Pr <cs.ammarn@gmail.com>
2018-07-12 11:21:24 +0300

    this is our first commmit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype>
+<html>
+<head>
+</head>
+<body>
+<?php
+require_once "Classes/PHPExcel.php";
+		$tmpfname = "Specs TLX 2015+(1).xlsx";
+		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+		$excelObj = $excelReader->load($tmpfname);
+		$worksheet = $excelObj->getSheet(0);
+		$lastRow = $worksheet->getHighestRow();
+		
+		echo "<table>";
+		for ($row = 1; $row <= $lastRow; $row++) {
+			 echo "<tr><td>";
+			 echo $worksheet->getCell('B'.$row)->getValue();
+			 echo "</td><td>";
+			 echo $worksheet->getCell('C'.$row)->getValue();
+			 
+			 echo "</td><tr>";
+			 
+			 echo $row;
+			 echo "";
+		}
+		echo "</table>";	
+		
+		// this is just for testing
+?>
+
+</body>
+</html>
